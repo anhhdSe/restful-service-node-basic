606f48a5d022d40edcd081f704688b6a152468ff
anhhd <anhhdtech@gmail.com>
2024-06-02 11:23:25 +0700

    Add middleware, config file, views engine

---
+++ routes/courses.js
@@ -0,0 +1,67 @@
+const express = require("express");
+// const app = express() // This will not work when you seprate the route in seprated module
+
+// instead of working with app object, we working with router object
+const router = express.Router();
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
+];
+
+const validateGenres = (genre) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+};
+
+router.get("/", (req, res) => {
+  res.send(genres);
+});
+
+router.get("/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with given ID was not found!");
+  res.send(genre);
+});
+
+router.post("/", (req, res) => {
+  const { error } = validateGenres(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+router.put("/:id", (req, res) => {
+  const { error } = validateGenres(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with given ID was not found");
+
+  genre.name = req.body.name;
+
+  res.send(genre);
+});
+
+router.delete("/:id", (req, res) => {
+  const { error } = validateGenres(req.body);
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with given ID was not found");
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
+// Basically we get the route object on the top. Then we add route to it and then export
+module.exports = router;
--- server.js
+++ server.js
@@ -1,67 +1,19 @@
 const express = require("express");
 const app = express();
 const Joi = require("joi");
-
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" },
-];
-
-const validateGenres = (genre) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  return schema.validate(genre);
-};
+const config = require("config");
+const genres = require("./routes/courses");
+const home = require("./routes/home");
+const helmet = require("helmet");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
 
-app.get("/api/genres", (req, res) => {
-  res.send(genres);
-});
-
-app.get("/api/genres/:id", (req, res) => {
-  const genre = genres.find((g) => g.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with given ID was not found!");
-  res.send(genre);
-});
-
-app.post("/api/genres", (req, res) => {
-  const { error } = validateGenres(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  genres.push(genre);
-  res.send(genre);
-});
-
-app.put("/api/genres/:id", (req, res) => {
-  const { error } = validateGenres(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = genres.find((g) => g.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with given ID was not found");
-
-  genre.name = req.body.name;
-
-  res.send(genre);
-});
-
-app.delete("/api/genres/:id", (req, res) => {
-  const { error } = validateGenres(req.body);
-  const genre = genres.find((g) => g.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with given ID was not found");
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
-});
+app.use("/", home);
+// Routes start with /api/genres will use genres router
+app.use("/api/genres", genres);
 
 const port = process.env.PORT || 3000;
 
